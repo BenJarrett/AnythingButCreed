f838bd7033eeb54a11864d8445d8c0216094197d
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-07 11:41:53 -0500

    Finished Assignment

--- AnythingButCreed/Program.cs
+++ AnythingButCreed/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AnythingButCreed
 {
@@ -6,7 +8,32 @@ namespace AnythingButCreed
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var goodSongs = new List<Song>();
+
+            var allSongs = new List<Song>()
+            { 
+            new Song("Creed", "Higher"),
+            new Song("Creed", "One Last Breath"),
+            new Song("Creed", "With Arms Wide Open"),
+            new Song("Creed", "My Sacrifice"),
+            new Song("Creed", "What's This Life For"),
+            new Song("Creed", "What If"),
+            new Song("Counterparts", "The Constant"),
+            new Song("Phoebe Bridgers", "Scott Street"),
+            new Song("All Get Out", "The Season"),
+            new Song("Samia", "Big Wheel"),
+            new Song("Samia", "As You Are"),
+            new Song("All Get Out", "My Friends"),
+            };
+
+            goodSongs.AddRange(allSongs.Where(song => song.Artist != "Creed"));
+
+            foreach (var song in goodSongs)
+            {
+
+                Console.WriteLine($"{song.Artist}:" +
+                    $"'{song.Name}'");
+            }
         }
     }
 }
